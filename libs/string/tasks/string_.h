4c1a8c4f301349e1646ceaa6a41670c7aac4458e
pixel627 <pixelguy627@gmail.com>
2024-05-12 20:11:17 +0300

    hasWordsWithSameLetters

--- libs/string/tasks/string_.h
+++ libs/string/tasks/string_.h
@@ -97,6 +97,8 @@ void assertString(const char *expected, char *got,char const *fileName, char con
 
 void findLastWord(char *s1, char *s2);
 
-int isEqual(char *s);
+int hasSameWords(char *s);
+
+int hasWordsWithSameLetters(char *s);
 
 #endif //INC_STRING__H
